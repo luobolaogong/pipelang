c67b0bc969ada106878c84dc8c7211c780ce32f4
rob <luobolaogong@gmail.com>
2022-05-17 18:30:32 -1000

    Computer failing.  Must checkin everything.

---
+++ dotunes.sh
@@ -0,0 +1,5 @@
+dart bin/ppl.dart -i tunes/AmberOnTheRocks.ppl -o midis/AmberOnTheRocks.mid
+dart bin/ppl.dart -i tunes/BanjoBreakdown.ppl -o midis/BanjoBreakdown.mid
+dart bin/ppl.dart -i tunes/CaptEwingChanter.ppl -o midis/CaptEwingChanter.mid
+dart bin/ppl.dart -i tunes/HawaiiAlohaChanterMelody.ppl -c 1 -o midis/HawaiiAlohaChanterMelody.mid
+dart bin/ppl.dart -i tunes/HawaiiAlohaChanterHarmony.ppl -o midis/HawaiiAlohaChanterHarmony.mid
